33bbdf0f1b3af1784f43883df8dfce050b71c25e
andrewLay <aqflay@uwaterloo.ca>
2021-02-15 21:33:25 -0500

    Update SELECT basics.sql

--- SELECT basics.sql
+++ SELECT basics.sql
@@ -1,7 +1,7 @@
 /*
                       world
-name	      continent	  area	  population	gdp
-Afghanistan	Asia	      652230	25500100	  20343000000
+name        continent	  area	  population	gdp
+Afghanistan Asia	      652230	25500100	  20343000000
 Albania	    Europe	    28748	  2831741	    12960000000
 Algeria	    Africa	    2381741	37100000	  188681000000
 Andorra	    Europe	    468	    78115	      3712000000
