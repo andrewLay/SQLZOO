a5be0f90e496d27f0b7f4a13590e9294c52d7a61
andrewLay <aqflay@uwaterloo.ca>
2021-02-18 22:17:38 -0500

    Create 7. Using NULL.sql

---
+++ 7. Using NULL.sql
@@ -0,0 +1,19 @@
+/*
+                 teacher
+id    dept  name        phone mobile
+101   1     Shrivell    2753  07986 555 1234
+102   1     Throd       2754  07122 555 1920
+103   1     Splint      2293	
+104         Spiregrain  3287	
+105   2     Cutflower   3212  07996 555 6574
+106         Deadyawn    3345  
+...
+
+                 dept
+id    name
+1     Computing
+2     Design
+3     Engineering
+...
+
+*/
